1a46e4b86e15031cbd37d75795727516af128658
Kundan Kumar <kundankr103060@gmail.com>
2023-05-16 01:06:20 +0530

    Mu Goprd

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 public class Main {
   public static void main(String [] args){
     System.out.println("My Code");
-    ;lk;l;l ;lkk; k;kk;lkl;k;l ;lk;lk;lk;lkl; l;k;lkl;kl;k;olk;l;l k;kl;
+   ;lkl;ll4 ;lk;l;l ;lkk; k;kk;lkl;k;l ;lk;lk;lk;lkl; l;k;lkl;kl;k;olk;l;l k;kl;
   }
 }
 
@@ -10,6 +10,6 @@ public class Main{
     public static void main(String[] args){
         System.out.println("Test");
     }
-    Test
+    Testnp  pm p
 }
 
